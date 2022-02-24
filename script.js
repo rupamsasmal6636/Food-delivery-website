eca93c1787b0d8b43cb625e1c05ee0ec076cbc04
Rupam Sasmal <rupam.sasmal.6636@gmail.com>
2022-02-24 22:01:44 +0530

    solve home container issue

---
+++ script.js
@@ -0,0 +1,9 @@
+const orderBtn=document.getElementById('order');
+orderBtn.addEventListener('click',()=>{
+    alert('Sorry for the inconvenience! Currently, We are not accepting any order')
+});
+
+const submitBtn=document.getElementById('submit');
+submitBtn.addEventListener('click',()=>{
+    alert('Successfully Submitted! Thank You!')
+})
